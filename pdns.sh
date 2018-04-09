2c8a56be85ec8dc6d286ad639a439a1677461a6b
Hugh Davenport <hugh@davenport.net.nz>
2018-04-10 11:59:49 +1200

    Fix up for latest dehydrated
    - Extend the list of commands to ignore
    - Don't try and get root domain, just set it on the domain given

--- pdns.sh
+++ pdns.sh
@@ -10,27 +10,17 @@ set -u
 set -o pipefail
 umask 077
 
-# Wait this value in seconds at max for all nameservers to be ready 
-# with the deployed challange or fail if they are not
-dns_sync_timeout_secs=90
-
-   domain="${2}"
-    token="${4}"
-
-IFS='.' read -a myarray_domain <<< "$domain"
-# Extract TLD from domain
-lastcmp=$(echo $domain | rev | cut -d "." -f2);
-if [ ${#lastcmp} == 2 ]; then
-    root_domain=$(echo $domain | rev | cut -d "." -f1-3 | rev) # ccTLDs
-    root_length=2
-else
-    root_domain=$(echo $domain | rev | cut -d "." -f1-2 | rev) # TLD
-    root_length=1
-fi
 done="no"
 
 if [[ "$1" = "deploy_challenge" ]]; then
-   pdnsutil add-record "${root_domain}" _acme-challenge TXT "${token}"
+   # Wait this value in seconds at max for all nameservers to be ready 
+   # with the deployed challange or fail if they are not
+   dns_sync_timeout_secs=90
+
+   domain="${2}"
+   token="${4}"
+   
+   pdnsutil add-record "${domain}" _acme-challenge TXT "${token}"
    domain_without_trailing_dot=${domain%.}
    dots=${domain_without_trailing_dot//[^.]}
    if [ "${#dots}" -gt $root_length ]; then
@@ -63,11 +53,13 @@ if [[ "$1" = "deploy_challenge" ]]; then
 fi
 
 if [[ "$1" = "clean_challenge" ]]; then
-    pdnsutil delete-rrset "${root_domain}" _acme-challenge TXT
+    domain="${2}"
+
+    pdnsutil delete-rrset "${domain}" _acme-challenge TXT
     done="yes"
 fi
 
-if [[ "${1}" = "deploy_cert" ]]; then
+if [[ "${1}" =~ ^(deploy_cert|deploy_ocsp|unchanged_cert|invalid_challenge|request_failure|generate_csr|startup_hook|exit_hook)$ ]]; then
     # do nothing for now
     done="yes"
 fi
